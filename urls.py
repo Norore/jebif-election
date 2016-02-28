f32c6cd2da5db69b9a7f4ac19574ee247cfb5972
Norore <nolwenn.lavielle@gmail.com>
2016-02-28 21:40:55 +0100

    django.views.generic.simple no longer exists, replace by the new function

--- urls.py
+++ urls.py
@@ -1,12 +1,12 @@
 
 from django.conf.urls import *
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 from election.views import *
 
 urlpatterns = patterns('',
 	('^(?P<election_id>\d+)/$', vote),
-	('^(?P<election_id>\d+)/ok/$', direct_to_template, {"template": "election/vote-ok.html"}),
+	('^(?P<election_id>\d+)/ok/$', TemplateView.as_view(template = "election/vote-ok.html")),
 	('^(?P<election_id>\d+)/results/$', results),
 	('^(?P<election_id>\d+)/mailing/$', mailing),
 )
