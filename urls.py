1dbb005072e20724debb54fc6eeb6386185371ef
Norore <nolwenn.lavielle@gmail.com>
2016-03-04 00:02:31 +0100

    Edit urlpatterns

--- urls.py
+++ urls.py
@@ -1,13 +1,15 @@
 
 from django.conf.urls import *
+from django.views.generic.simple import direct_to_template
 from django.views.generic import TemplateView
-
-from election.views import *
+from . import views
+#from election.views import *
 
 urlpatterns = patterns('',
-	('^(?P<election_id>\d+)/$', vote),
-	('^(?P<election_id>\d+)/ok/$', TemplateView.as_view(template = "election/vote-ok.html")),
-	('^(?P<election_id>\d+)/results/$', results),
-	('^(?P<election_id>\d+)/mailing/$', mailing),
+	('^(?P<election_id>\d+)/$', views.vote),
+	#('^(?P<election_id>\d+)/ok/$', TemplateView.as_view(template = "election/vote-ok.html")),
+        ('^(?P<election_id>\d+)/ok/$', direct_to_template{'template': 'election/vote-ok.html'}),
+	('^(?P<election_id>\d+)/results/$', views.results),
+	('^(?P<election_id>\d+)/mailing/$', views.mailing)
 )
 
