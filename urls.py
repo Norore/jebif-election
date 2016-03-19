6199031b4f5a8deeaf5059a77e84e7c3fdad34fa
Norore <nolwenn.lavielle@gmail.com>
2016-03-19 22:41:21 +0100

    Change login.html

--- urls.py
+++ urls.py
@@ -1,14 +1,14 @@
 
 from django.conf.urls import *
-from django.views.generic.simple import direct_to_template
+#from django.views.generic.simple import direct_to_template
 from django.views.generic import TemplateView
-from . import views
-#from election.views import *
+#from . import views
+from election import views
 
 urlpatterns = patterns('',
 	('^(?P<election_id>\d+)/$', views.vote),
-	#('^(?P<election_id>\d+)/ok/$', TemplateView.as_view(template = "election/vote-ok.html")),
-        ('^(?P<election_id>\d+)/ok/$', direct_to_template{'template': 'election/vote-ok.html'}),
+	('^(?P<election_id>\d+)/ok/$', TemplateView.as_view(template_name = "election/vote-ok.html")),
+        #('^(?P<election_id>\d+)/ok/$', direct_to_template{'template': 'election/vote-ok.html'}),
 	('^(?P<election_id>\d+)/results/$', views.results),
 	('^(?P<election_id>\d+)/mailing/$', views.mailing)
 )
